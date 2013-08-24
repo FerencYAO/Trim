7913a4241420f36c424a67540476831db131eb40
yao <yaolmh@126.com>
2013-08-24 12:28:48 +0800

    Initial Commit

---
+++ StringOpera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StringOpera
+//
+//  Created by yao on 13-8-24.
+//  Copyright (c) 2013年 yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
